0e41c8e4832b350141fa04b6de6992742c2bac6f
joseph8884 <josegzm08@gmail.com>
2023-05-28 15:32:48 -0500

    add jsonproperty to direccion empresa

--- src/main/java/Componentes/Empresa.java
+++ src/main/java/Componentes/Empresa.java
@@ -22,7 +22,7 @@ public class Empresa {
     @Size(min = 3, message = "El sitio web de la empresa debe tener al menos 3 caracteres")
     @NotEmpty(message = "El sitio web de la empresa no puede estar vacío")
     protected String sitioWebEmpresa;
-
+    @JsonProperty("direccionEmpresa")
     @Size(min = 3, message = "La dirección de la empresa debe tener al menos 3 caracteres")
     @NotEmpty(message = "La dirección de la empresa no puede estar vacía")
     protected String direccionEmpresa;
