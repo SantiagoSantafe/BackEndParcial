0d0cd5e5348befe464f16441e80b88e9701fabbf
Santiago Santafe <andressasi@unisabana.edu.co>
2023-05-27 22:28:02 -0500

    Try to fix jackson error

--- src/main/java/co/edu/unisabana/BackEndParcial/Practicas.java
+++ src/main/java/co/edu/unisabana/BackEndParcial/Practicas.java
@@ -2,6 +2,8 @@ package co.edu.unisabana.BackEndParcial;
 
 import javax.validation.constraints.*;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import Componentes.Empresa;
 import Componentes.Supervisor;
 import Componentes.Tarea;
@@ -13,14 +15,17 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Practicas {
+    @JsonProperty("empresa")
     @NotEmpty(message = "Ingrese todos los valores para la empresa")
     @NotNull(message = "No esta el campoo")
     protected Empresa empresa;
 
+    @JsonProperty("supervisor")
     @NotEmpty(message = "Ingrese todos los valores para supervisor")
     @NotNull(message = "No esta el campoo")
     protected Supervisor supervisor;
 
+    @JsonProperty("tarea")
     @NotEmpty(message = "Ingrese todos los valores para supervisor")
     @NotNull(message = "No esta el campoo")
     protected Tarea tarea;
